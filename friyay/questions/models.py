c3338fee8a8b76cc9ddc3b4e03bbad5ae22c9512
Pallavi Hukerikar <pallavihukerikar@Pallavis-MacBook-Pro.local>
2018-12-12 13:38:26 +0100

    object models

--- friyay/questions/models.py
+++ friyay/questions/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Meeting(models.Model):
+    name = models.CharField(max_length=150)
+    creation_time = models.DateTimeField()
+    password = models.CharField(max_length=100)
+    archived = models.BooleanField()
+    associated_questions = models.Choices()
 
-# class Meeting(models.Model):
-#     name = models.Ch
+# question, comment, or answer
+class Entry(models.Model):
 
+    entry_text = models.CharField()
+    creation_time = models.DateTimeField()
+    creator_name = models.CharField(max_length=150)
+    upvotes = models.PositiveSmallIntegerField()
+    downvotes = models.PositiveSmallIntegerField()
 
+class Question(Entry):
+    parent_id = models.ForeignKey(Meeting, on_delete=models.CASCADE)
 
-
-
-
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-#
-#
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
+class Response(Entry):
+    response_type_choices = (('question', 'Question'),
+                             ('comment', 'Comment')
+                             ('answer', 'Answer'))
+    response_type = models.CharField(choices= response_type_choices, default='comment')
+    parent_id = models.ForeignKey(Question, on_delete=models.CASCADE)
